2d2f030949a22470c137f6ec094770a38489e9bc
M. Auayan <sxtxixtxcxh@gmail.com>
2011-08-22 14:58:00 -0700

    updated graphic tiles

--- about_screen.lua
+++ about_screen.lua
@@ -7,6 +7,7 @@ end
 function AboutScreen:draw()
   love.graphics.setColor(255,255,255);
   love.graphics.printf("A game by Mike Judge", 0, 200, 800, 'center')
+  love.graphics.printf("Some graphics by M. Auayan", 0, 250, 800, 'center')
   love.graphics.printf("Press enter to return to the previous screen", 0, 400, 800, 'center')
 end
 
