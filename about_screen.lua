b9cf624a589cf01b272da893e1823f40e4349060
Mike Judge <mikelovesrobots@gmail.com>
2011-08-25 23:51:51 -0700

    Added Barton to the about screen

--- about_screen.lua
+++ about_screen.lua
@@ -6,8 +6,9 @@ end
 
 function AboutScreen:draw()
   love.graphics.setColor(255,255,255);
-  love.graphics.printf("A game by Mike Judge", 0, 200, 800, 'center')
-  love.graphics.printf("Some graphics by M. Auayan", 0, 250, 800, 'center')
+  love.graphics.printf("Programming and Concept by Mike Judge", 0, 200, 800, 'center')
+  love.graphics.printf("Graphics by M. Auayan", 0, 230, 800, 'center')
+  love.graphics.printf("Music by Barton McGuire", 0, 260, 800, 'center')
   love.graphics.printf("Press enter to return to the previous screen", 0, 400, 800, 'center')
 end
 
